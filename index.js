e9fa694966a8660928121d43508b0b1eaf799903
Sayaki-M <38837489+Sayaki-M@users.noreply.github.com>
2023-01-21 02:55:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { dirname } = require("path");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = process.env.PORT || 3001;
 
 app.use(express.static(__dirname + "/public"));
 
@@ -65,6 +66,6 @@ getAllRoomNames = async () => {
   return allRoomNames;
 };
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log("listening on *:3000");
 });
